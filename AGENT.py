86002f185bf1c250b763fe7e40bca34ca634ccd4
Pranav Kumar <96071514+PraNavKumAr01@users.noreply.github.com>
2024-05-04 11:46:33 +0530

    Update AGENT.py

--- AGENT.py
+++ AGENT.py
@@ -1,7 +1,8 @@
 import os
-from langchain_community.tools.tavily_search import TavilyAnswer
+from langchain_community.tools.tavily_search import TavilyAnswer 
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +12,7 @@ os.environ["GROQ_API_KEY"] = os.environ.get("GROQ_API_KEY")
 
 searchTool = TavilyAnswer()
 llm = ChatGroq(temperature=0, model_name="llama3-8b-8192")
+memory = ConversationBufferMemory()
 
 def get_llm_response(transcript):
     template = """
@@ -22,12 +24,19 @@ def get_llm_response(transcript):
         3. **Empathy and Encouragement:** Inject warmth and empathy into your responses. Acknowledge the customer's feelings, especially if they're frustrated or upset.
         4. **Instructions and Guidance:** For troubleshooting or setup guidance, provide step-by-step instructions, checking in with the customer at each step to ensure they're following along.
         5. **Feedback Queries:** Occasionally ask for feedback to confirm the customer is satisfied with the solution or needs further assistance.
+        6. **Converation Memory:** You will always receive the context of the conversation you are having with the user, this includes previous queries and your replies, make sure to hold the continuity by adhering to the chat history
         Your role is crucial in making the user experience outstanding. Let's make every interaction count! Always make sure to answer in minimum words and sentences to make it seem like a real conversation, avoid long answers and explanations
         ALWAYS ANSWER IN LESS THAN 30 WORDS
         You will also recieve some web search results extracted using the user query, use that information if and when you need to generate a good answer. You dont always have to use this information
     """
-    search_result = searchTool.run(transcript, verbose = True)
-    prompt = ChatPromptTemplate.from_messages([("human", template + "Web Search Results : {search_result}" +" User Query : {snippet}")])
+    search_result = searchTool.run(transcript)
+
+    prompt = ChatPromptTemplate.from_messages([
+            ("human", template + "Web Search Results : {search_result}" + " User Query : {snippet}" + " Chat History: {memory}")
+        ])
     chain = prompt | llm
-    answer = chain.invoke({"search_result" : search_result ,"snippet": transcript})
-    return answer.content
+
+    result = chain.invoke({"search_result": search_result, "snippet": transcript, "memory": memory.buffer})
+    memory.save_context({"input": transcript}, {"output" : result.content})
+
+    return result.content
